f18b486d09125a8c434478e08038e47d73e35de6
Nicholas Pachulski <nick@pachulski.me>
2026-05-12 04:02:25 -0400

    wip

---
+++ dotfiles/config/orchard-themes/catppuccin-macchiato/nvim.lua
@@ -0,0 +1,6 @@
+require("catppuccin").setup({
+  flavour = "macchiato",
+  transparent_background = require("theme_opacity").transparent(),
+})
+vim.opt.background = "dark"
+vim.cmd.colorscheme("catppuccin-macchiato")
